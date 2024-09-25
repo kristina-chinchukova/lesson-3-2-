39f948c805f45d8138cdba4fbb83668eb7cbb657
kristina-chinchukova <kristina.chinchukova@gmail.com>
2024-09-25 21:57:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 long = len(my_list)
-account = 0
+account = []
 number = 0
 while long != 0:
     if my_list[number] >= 0 :
         if my_list[number] > 0 :
-            account=account + 1
+            account.append(my_list[number])
         long = long - 1
         number = number + 1
         continue
