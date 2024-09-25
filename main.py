1512d7df412f45346e8f6fddd4dddce7d33911ff
kristina-chinchukova <kristina.chinchukova@gmail.com>
2024-09-25 13:56:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+long = len(my_list)
+account = 0
+number = 0
+while long != 0:
+    if my_list[number] >= 0 :
+        if my_list[number] > 0 :
+            account=account + 1
+        long = long - 1
+        number = number + 1
+        continue
+    break
+print(account)
